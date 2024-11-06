a91961dddf9c96f880052bfb13dcb5412fb97a20
Jacob Sutherland <69085706+jadau1313@users.noreply.github.com>
2024-11-06 14:57:59 -0800

    Add files via upload
    The body emits various biological signals. The eyes have polarity, which means that we can monitor the position of the eye based on a change in voltage. This means, that we can use our eyes to control things, including audio. To do this, one electrode must be connected to the outer canthus of an eye, the other can be connected to either the outer canthus of the other eye, or to the mastoid bone on the opposite side, and finally, a ground lead should be attached to the forehead. Note that different reference placements will result in different outcomes. The signals here are small, in the microvolts range, which is far too small for the arduino to be able to make use of. To solve this problem, I built a signal amplifier using an INA111 instrumentation amp and and a LM324 op amp along with an offset ground(arduino can't read negative voltages, so this is necessary). In this version, changing the position of the eyes changes the selection of notes along the pentatonic scale. The scale can be configured in many different ways, the ratios can be changed, and you could add MIDI output. You can also make note selection conditional using if statements, (ie if left, play A, if more left play B, if up play C, if right play scale, etc.), which would require calibration by reading the serial monitor values at different eye positions (everyone is going to be different, and electrode placement absolutely matters here).
    
    Now beyond synthesizers, this tech has utility in a number of applications, especially if the electrodes could be made to be part of a pair of glasses, including gesture controlled robotics, advanced prosthetics, smart home control, vehicle safety (detect drowsiness, sleeping, etc.), and of course, music and entertainment. 
    
    This is a work in progress. Future improvements will include using both eyes as input, which will require the construction of an additional amplifier circuit and figuring out how to share the offset ground with both amps (I am not an electrical engineer but I will figure this out), ability to switch scales and play modes using buttons, switches, and/or pots. Some of the cool parts about having both eyes as input(and referencing to opposite side mastoid) is that there will be increased accuracy when implementing conditional note selection choices and we could also add multiple outputs. Possibilities are endless. We can also add multiple outputs for one input, meaning that if we wanted to, we could use the eyes to output chords, or each eye could independently control selection from the pentatonic map if we are running this without conditional statements. 
    
    The design for the signal amplifier largely comes from watchmeflyy's "Controlling Lights With Your Eyes" project at https://www.instructables.com/Controlling-Lights-With-Your-Eyes/ (Many thanks for making this available!!!), and the audio design and code structure makes use of Peter Knight's Audiono http://code.google.com/p/tinkerit/wiki/Auduino (Again, many many thanks!).

---
+++ SynthesEYEzer_01.ino
@@ -0,0 +1,115 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+bool audioEnabled = false; // Global flag to control audio output
+const int buttonPin = 2;   // Pin connected to button
+
+
+uint16_t syncPhaseAcc;
+uint16_t syncPhaseInc;
+
+//map analog input
+#define EOG_SYNC_CONTROL  (1)
+#define PIN 3 //PWM to buzzer or audiojack
+const int EOGpin = A1; //anaolog pin for EOG
+
+
+#if defined(__AVR_ATmega8__)
+//
+// On old ATmega8 boards.
+//    Output is on pin 11
+//
+#define LED_PIN       13
+#define LED_PORT      PORTB
+#define LED_BIT       5
+#define PWM_PIN       11
+#define PWM_VALUE     OCR2
+#define PWM_INTERRUPT TIMER2_OVF_vect
+#elif defined(__AVR_ATmega1280__)
+//
+// On the Arduino Mega
+//    Output is on pin 3
+//
+#define LED_PIN       13
+#define LED_PORT      PORTB
+#define LED_BIT       7
+#define PWM_PIN       3
+#define PWM_VALUE     OCR3C
+#define PWM_INTERRUPT TIMER3_OVF_vect
+#else
+//
+// For modern ATmega168 and ATmega328 boards
+//    Output is on pin 3
+//
+#define PWM_PIN       3
+#define PWM_VALUE     OCR2B
+#define LED_PIN       13
+#define LED_PORT      PORTB
+#define LED_BIT       5
+#define PWM_INTERRUPT TIMER2_OVF_vect
+#endif
+
+// Stepped Pentatonic mapping
+//
+uint16_t pentatonicTable[54] = {
+  0,19,22,26,29,32,38,43,51,58,65,77,86,103,115,129,154,173,206,231,259,308,346,
+  411,461,518,616,691,822,923,1036,1232,1383,1644,1845,2071,2463,2765,3288,
+  3691,4143,4927,5530,6577,7382,8286,9854,11060,13153,14764,16572,19708,22121,26306
+};
+
+uint16_t mapPentatonic(uint16_t input) {
+  uint8_t value = (1023-input) / (1024/53);
+  return (pentatonicTable[value]);
+}
+
+void audioOn() {
+#if defined(__AVR_ATmega8__)
+  // ATmega8 has different registers
+  TCCR2 = _BV(WGM20) | _BV(COM21) | _BV(CS20);
+  TIMSK = _BV(TOIE2);
+#elif defined(__AVR_ATmega1280__)
+  TCCR3A = _BV(COM3C1) | _BV(WGM30);
+  TCCR3B = _BV(CS30);
+  TIMSK3 = _BV(TOIE3);
+#else
+  // Set up PWM to 31.25kHz, phase accurate
+  TCCR2A = _BV(COM2B1) | _BV(WGM20);
+  TCCR2B = _BV(CS20);
+  TIMSK2 = _BV(TOIE2);
+#endif
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(buttonPin, INPUT_PULLUP);
+  Serial.begin(115200);
+  pinMode(EOGpin, INPUT);
+  pinMode(PWM_PIN, OUTPUT);
+
+}
+
+void loop() {
+  audioEnabled = (digitalRead(buttonPin) == LOW); // Assuming LOW when button pressed
+  // put your main code here, to run repeatedly:
+  if(audioEnabled){
+    syncPhaseInc = mapPentatonic(analogRead(EOG_SYNC_CONTROL));
+  }
+}
+
+SIGNAL(PWM_INTERRUPT){
+  if(audioEnabled){
+    uint8_t value;
+    uint16_t output;
+    syncPhaseAcc+=syncPhaseInc;
+    value = (syncPhaseAcc >> 7) & 0xff; //convert to triangle
+    if (syncPhaseAcc & 0x8000) value = ~value; // Invert value for triangle wave
+
+    // Scale output to the available range, clipping if necessary
+    output = value;
+    if (output > 255) output = 255;
+    PWM_VALUE = output;
+  }else{
+    PWM_VALUE = 0;
+  }
+}
